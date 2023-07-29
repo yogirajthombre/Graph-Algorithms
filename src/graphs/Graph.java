1d4b26c0b6144357e2f85da4f55848d9be20c5ba
yogirajthombre93@gmail.com <yogirajthombre93@gmail.com>
2023-07-29 15:40:18 +0530

    Implemented Island Perimeter

--- src/graphs/Graph.java
+++ src/graphs/Graph.java
@@ -459,4 +459,45 @@ public class Graph {
         }
     }
 
+    /***
+     * 
+     * Island Perimeter 
+     * Time complexity : O(m*n)
+     * Space Complexity : O(1)
+     * 
+     * If there are adjacent ones then reduce the current Output by one 
+     *
+     */
+
+    public int islandPerimeter(int[][] grid) {
+        int output = 0;
+        for (int row = 0;row<grid.length;row++){
+            for (int col = 0;col<grid[0].length;col++){
+            if (grid[row][col] == 1){
+            
+            output += 4;
+
+            if (col-1 >= 0  && grid[row][col-1] == 1){
+                output -= 1;
+            } 
+
+            if (col+1 != grid[row].length && grid[row][col+1] == 1){
+                output -=1;
+            }  
+
+            if (row-1 >= 0 && grid[row-1][col] == 1){
+                output -= 1;
+            }   
+                    
+            if (row+1 != grid.length && grid[row+1][col] == 1){
+                output -=1;
+            } 
+
+            }
+            }
+        }
+
+        return output;
+    }
+
 }
